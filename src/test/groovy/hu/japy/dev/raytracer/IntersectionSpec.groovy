62c9df3aeae825101142ec323d22e857e443caeb
Gábor László Hajba <gabor.hajba@gmail.com>
2017-09-28 14:30:46 +0200

    added one missing test

--- src/test/groovy/hu/japy/dev/raytracer/IntersectionSpec.groovy
+++ src/test/groovy/hu/japy/dev/raytracer/IntersectionSpec.groovy
@@ -3,7 +3,10 @@ package hu.japy.dev.raytracer
 import spock.lang.Specification
 
 import static hu.japy.dev.raytracer.Intersection.intersection
+import static hu.japy.dev.raytracer.Ray.ray
 import static hu.japy.dev.raytracer.Sphere.sphere
+import static hu.japy.dev.raytracer.Tuple.point
+import static hu.japy.dev.raytracer.Tuple.vector
 
 
 class IntersectionSpec extends Specification {
@@ -19,6 +22,19 @@ class IntersectionSpec extends Specification {
         i.object == s
         and: "i.inside is true"
         i.inside
+    }
+
+    def "Intersect computes inside/outside correctly."() {
+        when: "r is ray(point(0, 0, -5), vector(0, 0, 1))"
+        def r = ray(point(0, 0, -5), vector(0, 0, 1))
+        and: "s is sphere()"
+        def s = sphere()
+        and: "i is intersect(r, s)"
+        def i = r.intersect(s)
+        then: "i1.inside = false"
+        !i[0].inside
+        and: "i2.inside = true"
+        i[1].inside
     }
 
 }
